bf24ef3a96b60ca7c0966f41adf2ddfb3824e129
vmms16 <vmms16@gmail.com>
2016-05-30 20:01:18 -0300

    Novos pacotes e Layout

--- app/src/main/java/makerprice/com/makerpriceufrpe/infra/DatabaseHelper.java
+++ app/src/main/java/makerprice/com/makerpriceufrpe/infra/DatabaseHelper.java
@@ -17,29 +17,28 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "makerprice.db";
 
 
+    public static final String TABLE_USER = "usuario";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_EMAIL = "email";
+    public static final String COLUMN_PASS = "pass";
 
-    private static final String TABLE_USER = "usuario";
-    private static final String COLUMN_ID = "_id";
-    private static final String COLUMN_NAME = "name";
-    private static final String COLUMN_EMAIL = "email";
-    private static final String COLUMN_PASS = "pass";
-
-    private static final String TABLE_USER_LOJA= "usuarioLoja";
-    private static final String COLUMN_CNPJ= "cnpj";
-    private static final String COLUMN_AJSON= "ajson";
-    private static final String COLUMN_LINKIMAGEM="linkImagem";
+    public static final String TABLE_USER_LOJA = "usuarioLoja";
+    public static final String COLUMN_CNPJ = "cnpj";
+    public static final String COLUMN_AJSON = "ajson";
+    public static final String COLUMN_LINKIMAGEM = "linkImagem";
 
 
-    public void setTabela(String tabela){
-        this.tabela=tabela;
+    public void setTabela(String tabela) {
+        this.tabela = tabela;
     }
 
-    public String getTabela(){
+    public String getTabela() {
         return this.tabela;
     }
 
-    public String criarTabelaUsuario(){
-        String ctu= "CREATE TABLE " + TABLE_USER + " (" +
+    public String criarTabelaUsuario() {
+        String ctu = "CREATE TABLE " + TABLE_USER + " (" +
                 COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
                 COLUMN_EMAIL + " TEXT NOT NULL, " +
                 COLUMN_NAME + " TEXT NOT NULL, " +
@@ -47,27 +46,28 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return ctu;
     }
 
-    public String criarTabelaUsuarioLoja(){
-        String ctul= "CREATE TABLE " + TABLE_USER_LOJA + " (" +
+    public String criarTabelaUsuarioLoja() {
+        String ctul = "CREATE TABLE " + TABLE_USER_LOJA + " (" +
                 COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
                 COLUMN_EMAIL + " TEXT NOT NULL, " +
                 COLUMN_NAME + " TEXT NOT NULL, " +
-                COLUMN_PASS + " TEXT NOT NULL);"+
-                COLUMN_CNPJ + "TEXT NOT NULL)"+
-                COLUMN_AJSON+ "TEXT NOT NULL"+
-                COLUMN_LINKIMAGEM+"TEXT NOT NULL);";
+                COLUMN_PASS + " TEXT NOT NULL);" +
+                COLUMN_CNPJ + "TEXT NOT NULL)" +
+                COLUMN_AJSON + "TEXT NOT NULL" +
+                COLUMN_LINKIMAGEM + "TEXT NOT NULL);";
         return ctul;
     }
-
 
 
     public DatabaseHelper(Context context, String tabela) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
 
-        switch (tabela){
-            case "usuario": this.setTabela(this.criarTabelaUsuario());
+        switch (tabela) {
+            case "usuario":
+                this.setTabela(this.criarTabelaUsuario());
                 break;
-            case "usuarioLoja": this.setTabela(this.criarTabelaUsuarioLoja());
+            case "usuarioLoja":
+                this.setTabela(this.criarTabelaUsuarioLoja());
                 break;
         }
     }
@@ -91,24 +91,15 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         this.onCreate(db);
     }
 
-    public static String getColumnEmail(){
+    public static String getColumnEmail() {
         return COLUMN_EMAIL;
     }
-    public static String getColumnNome(){
+
+    public static String getColumnNome() {
         return COLUMN_NAME;
     }
-    public static String getColumnSenha(){
+
+    public static String getColumnSenha() {
         return COLUMN_PASS;
     }
-    public static String getColumnID(){
-        return COLUMN_ID;
-    }
-
-    public static String getTableUser(){
-        return TABLE_USER;
-    }
-    public static String getTableUserLoja() { return  TABLE_USER_LOJA;}
-    public static String getColumnCnpj(){return COLUMN_CNPJ;}
-    public static String getColumnLinkimagem(){return COLUMN_LINKIMAGEM;}
-    public static String getColumnAjson(){return  COLUMN_AJSON;}
 }

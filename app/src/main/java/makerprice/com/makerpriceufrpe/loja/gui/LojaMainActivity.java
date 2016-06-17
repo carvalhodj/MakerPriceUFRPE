805846ce786101a062b266344211eaf53c8992c8
Daniel Carvalho <danieljosecarvalho@gmail.com>
2016-06-16 21:15:50 -0300

    - Logout em loja;

--- app/src/main/java/makerprice/com/makerpriceufrpe/loja/gui/LojaMainActivity.java
+++ app/src/main/java/makerprice/com/makerpriceufrpe/loja/gui/LojaMainActivity.java
@@ -3,6 +3,8 @@ package makerprice.com.makerpriceufrpe.loja.gui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,6 +12,7 @@ import makerprice.com.makerpriceufrpe.componente.gui.CadastroComponenteActivity;
 import makerprice.com.makerpriceufrpe.R;
 import makerprice.com.makerpriceufrpe.infra.Sessao;
 import makerprice.com.makerpriceufrpe.loja.dominio.Loja;
+import makerprice.com.makerpriceufrpe.usuario.gui.LoginActivity;
 
 public class LojaMainActivity extends AppCompatActivity {
     private Sessao sessao = Sessao.getInstancia();
@@ -24,6 +27,26 @@ public class LojaMainActivity extends AppCompatActivity {
         String nome = loja.getNome();
         tv.setText(nome);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_loja, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.botao_logout) {
+            sessao.reset();
+
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void telaComponente(View v) {

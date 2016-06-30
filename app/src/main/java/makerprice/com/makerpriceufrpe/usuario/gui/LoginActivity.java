c2044044d4925e77271b6d0538106ee6a5aaef79
Daniel Carvalho <danieljosecarvalho@gmail.com>
2016-06-30 14:13:28 -0300

    - Ajustes nas cores do texto; - Ajuste na cor da status bar;

--- app/src/main/java/makerprice/com/makerpriceufrpe/usuario/gui/LoginActivity.java
+++ app/src/main/java/makerprice/com/makerpriceufrpe/usuario/gui/LoginActivity.java
@@ -1,9 +1,12 @@
 package makerprice.com.makerpriceufrpe.usuario.gui;
 
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 
 import makerprice.com.makerpriceufrpe.infra.GuiUtil;
@@ -27,6 +30,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(ContextCompat.getColor(getApplicationContext(), R.color.DarkGray));
     }
 
     public void onButtonClick(View v){
